9644f0c47d598196aa72a0a7fa9d551e78c25c54
Lucas Rangel <155129810+lucasrangelps@users.noreply.github.com>
2026-06-09 22:34:08 -0300

    update next.config

--- codigo-fonte/next.config.ts
+++ codigo-fonte/next.config.ts
@@ -1,12 +1,5 @@
-import type { NextConfig } from 'next'
-import path from 'path'
+import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: {
-      root: path.resolve(__dirname),
-    },
-  },
-}
+const nextConfig: NextConfig = {};
 
-export default nextConfig
+export default nextConfig;
